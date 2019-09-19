effc400d8e9ccb79b14a8744052f165e46ade8c0
bluenevus <frank.tucker@microhealthllc.com>
2019-09-19 11:26:24 -0400

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 15 }.to_i
-threads threads_count, threads_count
-bind "unix:/home/bilel/Desktop/rails_projects/mindmap/tmp/bluebird2.sock"
-environment ENV.fetch("RAILS_ENV") { "production" }
-workers ENV.fetch("WEB_CONCURRENCY") { 6 }
-daemonize false
-pidfile '/home/bilel/Desktop/rails_projects/mindmap/tmp/pids/puma.pid'
+        threads threads_count, threads_count
+        bind "unix:/var/www/mindmap/tmp/rails.sock"
+        environment ENV.fetch("RAILS_ENV") { "production" }
+        workers ENV.fetch("WEB_CONCURRENCY") { 6 }
+        daemonize true
+        pidfile '/var/www/mindmap/tmp/pids/puma.pid'
