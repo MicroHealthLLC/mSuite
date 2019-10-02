231ab85df1423fb18efb893877fac0589c5115d0
bilel-kedidi <bilel.kedidi@gmail.com>
2019-10-02 16:45:19 +0100

    add pack

---
+++ public/packs/js/canvas_operations-68849a949a39c37c64cd.js
@@ -0,0 +1,249 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/packs/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./app/javascript/packs/canvas_operations.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./app/javascript/packs/canvas_operations.js":
+/*!***************************************************!*\
+  !*** ./app/javascript/packs/canvas_operations.js ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var canvas = document.getElementsByTagName('canvas')[0];
+canvas.width = 800;
+canvas.height = 600;
+var gkhead = new Image();
+
+window.onload = function () {
+  var ctx = canvas.getContext('2d');
+  trackTransforms(ctx);
+
+  function redraw() {
+    // Clear the entire canvas
+    var p1 = ctx.transformedPoint(0, 0);
+    var p2 = ctx.transformedPoint(canvas.width, canvas.height);
+    ctx.clearRect(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
+    ctx.save();
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
+    ctx.drawImage(gkhead, 0, 0);
+  }
+
+  redraw();
+  var lastX = canvas.width / 2,
+      lastY = canvas.height / 2;
+  var dragStart, dragged;
+  canvas.addEventListener('mousedown', function (evt) {
+    document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
+    lastX = evt.offsetX || evt.pageX - canvas.offsetLeft;
+    lastY = evt.offsetY || evt.pageY - canvas.offsetTop;
+    dragStart = ctx.transformedPoint(lastX, lastY);
+    dragged = false;
+  }, false);
+  canvas.addEventListener('mousemove', function (evt) {
+    lastX = evt.offsetX || evt.pageX - canvas.offsetLeft;
+    lastY = evt.offsetY || evt.pageY - canvas.offsetTop;
+    dragged = true;
+
+    if (dragStart) {
+      var pt = ctx.transformedPoint(lastX, lastY);
+      ctx.translate(pt.x - dragStart.x, pt.y - dragStart.y);
+      redraw();
+    }
+  }, false);
+  canvas.addEventListener('mouseup', function (evt) {
+    dragStart = null;
+    if (!dragged) zoom(evt.shiftKey ? -1 : 1);
+  }, false);
+  var scaleFactor = 1.1;
+
+  var zoom = function zoom(clicks) {
+    var pt = ctx.transformedPoint(lastX, lastY);
+    ctx.translate(pt.x, pt.y);
+    var factor = Math.pow(scaleFactor, clicks);
+    ctx.scale(factor, factor);
+    ctx.translate(-pt.x, -pt.y);
+    redraw();
+  };
+
+  var handleScroll = function handleScroll(evt) {
+    var delta = evt.wheelDelta ? evt.wheelDelta / 40 : evt.detail ? -evt.detail : 0;
+    if (delta) zoom(delta);
+    return evt.preventDefault() && false;
+  };
+
+  canvas.addEventListener('DOMMouseScroll', handleScroll, false);
+  canvas.addEventListener('mousewheel', handleScroll, false);
+};
+
+gkhead.src = 'http://phrogz.net/tmp/gkhead.jpg'; // Adds ctx.getTransform() - returns an SVGMatrix
+// Adds ctx.transformedPoint(x,y) - returns an SVGPoint
+
+function trackTransforms(ctx) {
+  var svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+  var xform = svg.createSVGMatrix();
+
+  ctx.getTransform = function () {
+    return xform;
+  };
+
+  var savedTransforms = [];
+  var save = ctx.save;
+
+  ctx.save = function () {
+    savedTransforms.push(xform.translate(0, 0));
+    return save.call(ctx);
+  };
+
+  var restore = ctx.restore;
+
+  ctx.restore = function () {
+    xform = savedTransforms.pop();
+    return restore.call(ctx);
+  };
+
+  var scale = ctx.scale;
+
+  ctx.scale = function (sx, sy) {
+    xform = xform.scaleNonUniform(sx, sy);
+    return scale.call(ctx, sx, sy);
+  };
+
+  var rotate = ctx.rotate;
+
+  ctx.rotate = function (radians) {
+    xform = xform.rotate(radians * 180 / Math.PI);
+    return rotate.call(ctx, radians);
+  };
+
+  var translate = ctx.translate;
+
+  ctx.translate = function (dx, dy) {
+    xform = xform.translate(dx, dy);
+    return translate.call(ctx, dx, dy);
+  };
+
+  var transform = ctx.transform;
+
+  ctx.transform = function (a, b, c, d, e, f) {
+    var m2 = svg.createSVGMatrix();
+    m2.a = a;
+    m2.b = b;
+    m2.c = c;
+    m2.d = d;
+    m2.e = e;
+    m2.f = f;
+    xform = xform.multiply(m2);
+    return transform.call(ctx, a, b, c, d, e, f);
+  };
+
+  var setTransform = ctx.setTransform;
+
+  ctx.setTransform = function (a, b, c, d, e, f) {
+    xform.a = a;
+    xform.b = b;
+    xform.c = c;
+    xform.d = d;
+    xform.e = e;
+    xform.f = f;
+    return setTransform.call(ctx, a, b, c, d, e, f);
+  };
+
+  var pt = svg.createSVGPoint();
+
+  ctx.transformedPoint = function (x, y) {
+    pt.x = x;
+    pt.y = y;
+    return pt.matrixTransform(xform.inverse());
+  };
+}
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=canvas_operations-68849a949a39c37c64cd.js.map
