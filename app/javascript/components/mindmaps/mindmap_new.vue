af9bca69b08aa78d26afe411f04271e94f4152fc
LAlexMHllc <LAlexMHllc@users.noreply.github.com>
2019-11-08 14:09:36 -0500

    Update mindmap_new.vue

--- app/javascript/components/mindmaps/mindmap_new.vue
+++ app/javascript/components/mindmaps/mindmap_new.vue
@@ -7,7 +7,7 @@
             href="javascript:;"
             role="button" 
             class="d-flex text-info edit_delete_btn delete ml-2 mr-3 center_flex"
-            @click.stop="goHome" 
+            @click.stop="$refs.homeMapModal.open" 
           >
             <i class="material-icons home_icon icons d-flex center_flex"></i>
           </a> 
@@ -159,7 +159,34 @@
           </input-field>
         <canvas id="map-canvas" class="has_bg" :width="windowWidth" :height="windowHeight" @click.stop="nullifyFlags"></canvas>
       </section>
-    </div>
+    </div>    
+    <sweet-modal ref="homeMapModal" class="of_v">
+      <div class="sweet_model_icon_div">
+        <div class="radius_circle bg-warning center_flex mlr_a">
+          <i class="material-icons text-white">error_outline</i>
+        </div>
+      </div>
+      <h3 class="f_smooth_auto">Did you save this Map Key?</h3>
+      <p class="text-muted fs_18">You will need the key to view the map again!</p>
+      <div class="center_flex mt_2">
+        <a 
+          href="javascript:;" 
+          class="btn_2 bg-danger text-white mr_1"
+          @click.stop="goHome" 
+        >
+          <i class="material-icons mr-1">done</i>
+          Continue
+        </a>
+        <a 
+          href="javascript:;" 
+          class="btn_2 bg-primary text-white mr_1"
+          @click.stop="$refs.homeMapModal.close" 
+        >
+          <i class="material-icons mr-1">cancel</i>
+          Cancel
+        </a>
+      </div>
+    </sweet-modal>    
     <sweet-modal ref="newMapModal" class="of_v">
       <div class="sweet_model_icon_div">
         <div class="radius_circle bg-warning center_flex mlr_a">
