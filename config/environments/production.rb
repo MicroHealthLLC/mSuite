a60340753af44d6e09ae05a428a54f85da1bc6c6
Bilel <bilel.kedidi@gmail.com>
2021-11-25 19:14:27 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   # config.web_socket_server_url = 'ws://localhost/cable'
-  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  #config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  config.action_cable.disable_request_forgery_protection = true
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
