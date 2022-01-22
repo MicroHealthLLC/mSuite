fb63a5c54ba37d73465aa996a76955f2e69f1235
root <root@HP-ENVY-188.localdomain>
2022-01-22 14:50:32 -0500

    Started UI enhancements

--- app/javascript/components/home_page.vue
+++ app/javascript/components/home_page.vue
@@ -1,29 +1,39 @@
 <template>
-  <div class="main_div">
-    <h1 class="main_heading">mSuite</h1>
-    <div class="box_shadow box main_box">
-      <div class="w-100 text-center mb-3">
-         <input type="text" v-model="mindmapName" class="w-100 mindmap-name rounded" placeholder="Enter name of Concept Map here then select map type below">
-      </div>
-      <div class="row container pl-0">
-        <div v-for="type in mindmapTypes" class="col-lg-3 col-sm-6 col-md-4">
-          <div class="item p-0 my-2" @mouseover.self="hovered = type.key" @mouseleave.self="hovered = false" :class="hovered === type.key ? 'hovering' : ''">
-            <img :src="type.imgsrc" class="mindmap-img-size" @click.prevent="mindMapCreate(type.key)"
-            />
-            <span class="text-center">{{type.value}}</span>
+  <div>
+    <div class="main_div">
+      <div class="container-fluid">
+        <!-- //SEARCHBAR ROW -->
+         <div class="row pl-0 searchbar-row">
+            <div class="col text-center">
+              <span class="mx-auto text-light"><h1 class="mb-3 msuite-title d-inline px-3">mSuite</h1></span>
+            <div class="w-50 mx-auto my-4">
+              <span class="text-light searchbar-cta">TYPE YOUR CONCEPT TITLE HERE, THEN SELECT MAP TYPE BELOW</span>
+              <input type="text" v-model="mindmapName" class="w-100 mindmap-name rounded" placeholder="YOUR AWESOME CONCEPT TITLE">
+            </div>
+            </div>
+          </div>
+          <!-- Row for wrapper containing grid-layout mindmap items -->
+        <div class="row mx-auto">
+          <div v-for="type, i in mindmapTypes" class="mx-auto py-auto col-lg-2 col-sm-6 col-md-2 icon-col" :key="i">
+            <!-- <div class="item p-0 my-2 px-4" @mouseover.self="hovered = type.key" @mouseleave.self="hovered = false" :class="hovered === type.key ? 'hovering' : ''"> -->
+              <div class="item icon-item">
+              <img :src="type.imgsrc" class="mindmap-img-size" @click.prevent="mindMapCreate(type.key)"
+              />
+              <span class="text-center">{{type.value}}</span>
+            </div>
           </div>
         </div>
       </div>
+      <sweet-modal ref="errorModal" class="of_v" icon="error">
+        {{ errorMsg }}
+        <button v-if="oldMSuiteName.length > 9 && oldMSuiteName.length < 21" slot="button" class="btn btn-secondary mr-2" @click="continueMSuite()">Open</button>
+        <button slot="button" class="btn btn-secondary mr-2" @click="tryAgain()">Try Again</button>
+        <button slot="button" class="btn btn-info" @click="mindMapCreate(selectedType)">Create Random URL</button>
+      </sweet-modal>
+      <footer>
+      <cookie-law theme="dark-lime"></cookie-law>
+    </footer>
     </div>
-    <sweet-modal ref="errorModal" class="of_v" icon="error">
-      {{ errorMsg }}
-      <button v-if="oldMSuiteName.length > 9 && oldMSuiteName.length < 21" slot="button" class="btn btn-secondary mr-2" @click="continueMSuite()">Open</button>
-      <button slot="button" class="btn btn-secondary mr-2" @click="tryAgain()">Try Again</button>
-      <button slot="button" class="btn btn-info" @click="mindMapCreate(selectedType)">Create Random URL</button>
-    </sweet-modal>
-    <footer>
-    <cookie-law theme="dark-lime"></cookie-law>
-  </footer>
   </div>
 </template>
 
@@ -90,14 +100,13 @@
 <style scoped lang="css">
   .main_div {
     position: absolute;
-    height: 100%;
+    height: 80%;
     width: 100%;
-    background-image: linear-gradient(grey, white);
-  }
-  .main_heading {
-    font-family: 'Montserrat', sans-serif;
-    margin: 8vh;
-    text-align: center;
+    background: url('../../assets/images/landing_bg.jpg') no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
   }
   .main_box {
     margin-left: auto;
@@ -107,9 +116,17 @@
     background-image: linear-gradient(white, grey);
     box-shadow: 0 5px 5px rgba(0, 0, 0, 0.8);
   }
+  .searchbar-row{
+    margin: 7rem auto 12rem auto;
+  }
   .new_button_container {
     display: flex;
     justify-content: center;
+  }
+  .searchbar-cta{
+    font-size: 1.4rem;
+    margin-top: 20px;
+    font-family: Arial Narrow, sans-serif;
   }
   .new_button {
     color: white;
@@ -122,6 +139,20 @@
   .input_label {
     text-align: center;
     margin-top: 5%;
+  }
+  .icon-item {
+  position: absolute;
+  top: 50%;
+  left: 35%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+  }
+  .msuite-title {
+    font-family: sans-serif;
+    font-weight: 600;
+    border: solid #fff 4px;
+    border-radius: 4px;
+    padding: 5px;
   }
   .add_icon_span {
     font-weight: normal;
@@ -136,10 +167,25 @@
     border-top-left-radius: 0.25rem;
     border-bottom-left-radius: 0.25rem;
   }
+
   #basic-text1 {
     border-radius: 0.25rem;
     border-top-left-radius: 0 !important;
     border-bottom-left-radius: 0 !important;
+  }
+  .icon-col{
+    /* Each mindmap icon wrapper */
+    /* background-color: rgba(255, 255, 255, 0.8); */
+    background-color: #fff;
+    padding: 8px;
+    min-height: 200px;
+    border-radius: 4px;
+    transition: all .2s ease-in-out;
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  }
+  .icon-col:hover{
+    /* Each mindmap icon wrapper */
+    transform: scale(1.15);
   }
   .mind-map-list-header {
     text-align: center;
@@ -195,6 +241,12 @@
     display: inline-block;
     text-align: center;
     cursor: pointer;
+  }
+  .each-icon-wrapper{
+    transition: all .2s ease-in-out;
+  }
+  .each-icon-wrapper:hover{
+   transform: scale(1.3);
   }
   .mindmap-name {
     background-color: white;
