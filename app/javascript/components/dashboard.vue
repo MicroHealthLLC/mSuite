8dd13e8e41a13174dc4ba11534ad913c671281bd
b-solution <bilel.kedidi@gmail.com>
2022-01-25 21:44:52 +0500

    Worked on Cookies

--- app/javascript/components/dashboard.vue
+++ app/javascript/components/dashboard.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
     <router-view></router-view>
+    <cookie-law theme="dark-lime"></cookie-law>
+
   </div>
 </template>
+<script>
+  import CookieLaw from 'vue-cookie-law'
+  export default {
+      components: { CookieLaw }
+    }
+</script>
