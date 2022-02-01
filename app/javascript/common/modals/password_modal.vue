d38983947ec6ce20ceb7327641a7cf1dcf47c502
b-solution <bilel.kedidi@gmail.com>
2022-02-01 15:51:18 +0500

    Added PASSWORD Modal 'Back to Home' button, so user has option to return to Home Screen

--- app/javascript/common/modals/password_modal.vue
+++ app/javascript/common/modals/password_modal.vue
@@ -9,6 +9,7 @@
     <div>
       <input type="password" v-model="password" class="form-control" placeholder="Insert Password to Access Mindmap" autocomplete="new-password"/>
     </div>
+    <button slot="button" @click="goHome" class="btn btn-info mr-2"> Back To Home </button>
     <button slot="button" @click="private_password" class="btn btn-success"> Verify </button>
   </sweet-modal>
 </template>
@@ -26,6 +27,9 @@
         this.$emit("passwordCheck", this.password)
         this.password = ""
         this.$refs['passwordModal'].close();
+      },
+      goHome(){
+        window.open("/", "_self")
       }
     }
   }
