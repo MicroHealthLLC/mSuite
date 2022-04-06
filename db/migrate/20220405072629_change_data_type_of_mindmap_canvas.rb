d63b4e05df2266fe8cb91e4f318fbb1d85b1a7f8
b-solution <bilel.kedidi@gmail.com>
2022-04-06 16:39:58 +0500

    Worked on canvas migration issue.

--- db/migrate/20220405072629_change_data_type_of_mindmap_canvas.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataTypeOfMindmapCanvas < ActiveRecord::Migration[5.2]
-  def change
-    change_column :mindmaps, :canvas, :longtext
-  end
-end
--- db/migrate/20220406113251_add_canvas_to_mindmap.rb
+++ db/migrate/20220406113251_add_canvas_to_mindmap.rb
@@ -1,5 +1,5 @@
 class AddCanvasToMindmap < ActiveRecord::Migration[5.2]
   def change
-    add_column :mindmaps, :canvas, :json
+    add_column :mindmaps, :canvas, :longtext
   end
 end
