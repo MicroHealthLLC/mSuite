d5957d49b2222fb098c6451bf8aacdca50200b45
b-solution <bilel.kedidi@gmail.com>
2022-04-13 11:38:37 +0500

    Worked on if there are no values configured for captcha, then captcha should be disabled.

--- app/javascript/components/dashboard.vue
+++ app/javascript/components/dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Recaptcha v-if="!fromCaptcha"></Recaptcha>
+    <Recaptcha v-if="is_config && !fromCaptcha"></Recaptcha>
     <router-view v-if="fromCaptcha"></router-view>
     <cookie-law theme="dark-lime"></cookie-law>
 
@@ -12,12 +12,14 @@
   export default {
     data(){
       return{
-        fromCaptcha: false
+        fromCaptcha: false,
+        is_config: true
       }
     },
     components: { CookieLaw, Recaptcha },
     mounted(){
-      this.fromCaptcha = this.$cookies.get('verifiedCaptcha')
+      if(this.$cookies.get('verifiedCaptcha') == 'true') this.fromCaptcha = true
+      if(Vue.prototype.$google_recaptcha_site_key == "") this.is_config = false
     },
   }
 </script>
