21faee51a4bb24118f957630fbace8f4dea5f2f0
b-solution <bilel.kedidi@gmail.com>
2022-08-04 20:02:21 +0500

    Calendar: Recurring events in progress

--- app/javascript/common/modals/recurring_calendar_event_modal.vue
+++ app/javascript/common/modals/recurring_calendar_event_modal.vue
@@ -8,11 +8,11 @@
     <h3 class="f_smooth_auto">Add Recurrence</h3>
     <div>
       <span>
-        <p>Repeat Every <input type="number" value="1" class="w-10"/>
-          <select name="cars" id="cars">
-            <option value="Week">Week</option>
+        <p>Repeat Every <input type="number" v-model="repeatTime" min="1" max="100" class="w-10"/>
+          <select name="repeat" id="repeat" v-model="repeatType">
+            <option disabled value="">Select option</option>
+            <option value="Week" >Week</option>
             <option value="Month">Month</option>
-            <option value="Year">Year</option>
           </select>
         </p> 
       </span>
@@ -31,18 +31,23 @@
       <span>
         <p>Ends</p>
       <div>
-        <input type="radio" id="never" name="drone" value="never"checked>
+        <input type="radio" id="never" name="drone" value="never"checked v-model="endsOn">
         <label for="never">Never</label>
       </div>
 
     <div>
-      <input type="radio" id="on" name="drone" value="on">
+      <input type="radio" id="on" name="drone" value="on" v-model="endsOn">
       <label for="on">On</label>
+      <date-picker 
+        v-model="endOnDate" 
+        :format="format"
+      ></date-picker>
     </div>
 
     <div>
-      <input type="radio" id="after" name="drone" value="after">
+      <input type="radio" id="after" name="drone" value="after" v-model="endsOn">
       <label for="after">After</label>
+      <input type="number" value="1" class="w-10" v-model="occurences"/> Occurences
     </div>
       </span>
     </div>    
@@ -50,7 +55,7 @@
       <a
         href="javascript:;"
         class="btn_2 bg-danger text-white mr_1"
-        @click.stop="deleteMindmap"
+        @click.stop="calculateRecurrence"
       >
         <i class="material-icons mr-1">done</i>
         Continue
@@ -68,15 +73,56 @@
 </template>
 
 <script>
+  import DatePicker from 'vue2-datepicker';
+  import 'vue2-datepicker/index.css';
+
   export default {
     Name: "RecurringCalendarEventModal",
+    data () {
+      return{
+        repeatTime: 1,
+        repeatType: 'Week',
+        endsOn: null,
+        occurences: 1,
+        endOnDate: new Date(),
+        format: 'YYYY-MM-DD',
+        events: []
+      }
+    },
+    components: { DatePicker },
     methods: {
-      deleteMindmap() {
-        this.$emit('delete-mindmap')
+      calculateRecurrence() {
+        let eventDate = new Date()
+        if (this.repeatType == "Month"){
+          this.calculateMonthRecurrence(eventDate)
+        }
         this.closeMapModal()
       },
       closeMapModal() {
-        this.$refs.deleteMapModal.close()
+        this.$refs.RecurringCalendarEventModal.close()
+      },
+      calculateMonthRecurrence(eventDate){
+        let newEvents = []
+        let i = 0
+        let myDate = null
+        if (this.endsOn == "never"){
+          while(12-i >=this.repeatTime){
+            myDate = new Date(eventDate.setMonth(eventDate.getMonth() + parseInt(this.repeatTime)))
+            newEvents.push(myDate)
+            i = i + parseInt(this.repeatTime)
+          }
+        }
+        else if(this.endsOn == "on"){
+          if (this.endOnDate.getTime() > eventDate.getTime()){
+            while(12-i >=this.repeatTime){
+              myDate = new Date(eventDate.setMonth(eventDate.getMonth() + parseInt(this.repeatTime)))
+              newEvents.push(myDate)
+              i = i + parseInt(this.repeatTime)
+            }
+          }
+        }
+
+        this.events = newEvents
       }
     }
   }
@@ -86,5 +132,8 @@
   background-color:powderblue;
   border-radius: 50%;
   padding: 1vh 1vh;
-}
+  }
+  .mx-datepicker-popup {
+    z-index: 9001 !important;
+  }
 </style>
