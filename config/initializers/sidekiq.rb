9ba2c8e60a803411ce9544d8007c16e97a562c8e
b-solution <bilel.kedidi@gmail.com>
2022-09-07 19:56:10 +0500

    redis: fixed url issue in sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,12 @@
+require 'yaml'
+
 schedule_file = 'config/schedule.yml'
+cable_array = YAML.load(File.read("config/cable.yml"))
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: cable_array["redis"]["url"] } if Rails.env.development? || Rails.env.test?
+  config.redis = { url: cable_array["production"]["url"] } if Rails.env.production?
+end
 
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
