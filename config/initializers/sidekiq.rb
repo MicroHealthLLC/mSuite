b4f7e9c8d40211e6f646b489c617d591092c5c80
b-solution <bilel.kedidi@gmail.com>
2022-09-13 12:04:26 +0500

    Sidekiq: Fixed for client side redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,6 +8,11 @@ Sidekiq.configure_server do |config|
   config.redis = { url: cable_array["production"]["url"] } if Rails.env.production?
 end
 
+Sidekiq.configure_client do |config|
+  config.redis = { url: cable_array["redis"]["url"] } if Rails.env.development? || Rails.env.test?
+  config.redis = { url: cable_array["production"]["url"] } if Rails.env.production?
+end
+
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
