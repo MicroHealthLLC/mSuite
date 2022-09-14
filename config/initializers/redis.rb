233c4a8a5a3a86954ab3fdc0c35e77eda95f5b3d
Bilel.Kedidi <bilel.kedidi@gmail.com>
2022-09-14 08:05:05 +0100

    Use cable.yml to get redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,7 +2,10 @@ if Rails.env.test?
   require 'mock_redis'
   $redis = MockRedis.new
 else
-  $redis = Redis.new
+  cable_file = File.join(File.dirname(__FILE__), "../cable.yml")
+  yaml_config = ERB.new(IO.read(cable_file)).result
+  cable_config = YAML.respond_to?(:unsafe_load) ? YAML.unsafe_load(yaml_config) : YAML.load(yaml_config)
+  $redis =   Redis.new( url: cable_config[Rails.env.to_s ]['url'] ||  cable_config['redis']['url'] )
 end
 
 
