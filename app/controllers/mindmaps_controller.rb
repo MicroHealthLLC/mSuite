77c6575e547a88f630ff47a95ca014785d59ca65
b-solution <bilel.kedidi@gmail.com>
2022-10-14 19:18:24 +0500

    Fixed clicking 'Duplicate' on Kanban should create duplicate map

--- app/controllers/mindmaps_controller.rb
+++ app/controllers/mindmaps_controller.rb
@@ -181,7 +181,9 @@ class MindmapsController < AuthenticatedController
 
   def clone_map
     clone_msuite = @mindmap.amoeba_dup
+    clone_msuite.stages.skip_callback(:create, :before,:set_position)
     if clone_msuite.save
+      clone_msuite.stages.set_callback(:create, :before,:set_position)
       render json: { mindmap: clone_msuite.to_json, deleteAfter: ENV['DELETE_AFTER'].to_i, defaultDeleteDays: ENV['MAX_EXP_DAYS'].to_i, expDays: ENV['EXP_DAYS'].to_i }
     else
       render json: { mindmap: clone_msuite.to_json, messages: @mindmap.errors.full_messages, errors: clone_msuite.errors.to_json }, status: :found
