046697885ab9cd4fa97c1fade82ea2869774fb7c
b-solution <bilel.kedidi@gmail.com>
2022-10-19 12:43:23 +0500

    Fixed Coments Duplication

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 class Comment < ApplicationRecord
   include ActiveModel::Dirty
-  belongs_to :mindmap, optional: true
+  belongs_to :mindmap
+  has_many   :replies, class_name: 'Comment', foreign_key: 'parent_comment'
+  belongs_to :parent, class_name: 'Comment', foreign_key: 'parent_comment', optional: true
+  
   validates :message, presence: true
 end
