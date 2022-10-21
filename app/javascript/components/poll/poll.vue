58462727163cb4163b3f726a332331bf0c0ea63b
b-solution <bilel.kedidi@gmail.com>
2022-10-21 15:49:49 +0500

    Fixed poll map Temporary user

--- app/javascript/components/poll/poll.vue
+++ app/javascript/components/poll/poll.vue
@@ -93,12 +93,15 @@
         this.getData()
         if(this.dataLoaded) this.exportXLS(0)
       }
-      if (this.currentMindMap.canvas == null){
+      if (this.currentMindMap.canvas == null ){
         this.$store.dispatch('setUserEdit', this.currentMindMap.canvas)
         this.$store.dispatch('setTemporaryUser', this.currentMindMap.canvas)
       }
       this.sendLocals(false)
-      if (this.$store.getters.getMsuite.canvas != '{"version":"4.6.0","columns":[], "data":[], "style":{}, "width": []}' && this.$store.getters.getMsuite.canvas && JSON.parse(this.$store.getters.getMsuite.canvas).user) this.$store.dispatch('setUserEdit', JSON.parse(this.$store.getters.getMsuite.canvas).user)
+      if (this.$store.getters.getMsuite.canvas != '{"version":"4.6.0","columns":[], "data":[], "style":{}, "width": []}' && this.$store.getters.getMsuite.canvas && JSON.parse(this.$store.getters.getMsuite.canvas).user)
+      {
+        this.$store.dispatch('setUserEdit', JSON.parse(this.$store.getters.getMsuite.canvas).user)
+      }  
       else this.$store.dispatch('setUserEdit', null)
       this.$store.dispatch('setMindMapId', this.$store.getters.getMsuite.id)
       this.getUserOnMount()
