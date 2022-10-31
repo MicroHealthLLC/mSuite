51906d7b2cef9e6ff30897bae930212767b16a39
b-solution <bilel.kedidi@gmail.com>
2022-10-31 16:38:35 +0500

    Poll Vote: Export Image Fixed issue # 956

--- app/javascript/common/navigation_bar.vue
+++ app/javascript/common/navigation_bar.vue
@@ -422,8 +422,10 @@ export default {
       this.$store.dispatch('updateMSuite', obj)
     },
     putMSuite(value) {
-      this.$store.dispatch('updateMSuite', { mindmap: { title: value, canvas: this.$store.getters.getUser } })
       this.sendLocals(false)
+      this.checkMindmapType()
+      this.currentMindMap.title = value
+      this.$store.dispatch('updateMSuite', this.currentMindMap)
     },
     openUserModal() {
       this.$refs['user-box-modal'].$refs['UserBoxModal'].open()
@@ -543,7 +545,7 @@ export default {
         if (this.mm_type === 'spreadsheet') {
           elm = document.getElementsByClassName('jexcel_content')[0]
         }
-        if (this.mm_type == 'poll') {
+        if (this.mm_type == 'poll' || this.mm_type == 'pollvote') {
           let inner_elm = document.getElementById('poll-title')
           if (inner_elm) inner_elm.classList.remove("d-none");
         }
@@ -570,8 +572,12 @@ export default {
               });
               this.exportLoading = false
             }
-            _this.mm_type === 'kanban' ? document.getElementsByClassName('drag-inner-list').forEach(i => i.classList.remove('mh-100')) : false
-            _this.mm_type === 'poll' && document.getElementById('poll-title') ? document.getElementById('poll-title').classList.add('d-none') : false
+            if (_this.mm_type === 'kanban') {
+             document.getElementsByClassName('drag-inner-list').forEach(i => i.classList.remove('mh-100'))
+            }
+            if ((_this.mm_type === 'poll' || _this.mm_type == 'pollvote') && document.getElementById('poll-title')) {
+                document.getElementById('poll-title').classList.add('d-none')
+            }
             _this.$refs['exportOption'].close()
           })
           .catch((err) => {
