42580fc25104032e9fe3f824f9d9b04d34708a30
Amand Singh <amand.singh@microhealthllc.com>
2022-11-14 14:49:09 -0500

    Fix setting error

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -11,9 +11,22 @@ class Setting < ApplicationRecord
   rescue
   end
 
+  def setting_keys
+    ['office365_key', 'office365_secret', 'google_oauth_key', 'google_oauth_secret', 'failed_password_attempts', 'lockout_period', 'permanent_lock']
+  end
+
+  def as_json(options={})
+    h = {}
+    cached = Setting.first || new
+    setting_keys.each do |key|
+      h["#{key}"] = cached.send(key) ? cached.send(key) : (ENV["#{key.upcase}"] || '').to_i
+    end
+    h
+  end
+  
   def self.load_available_settings
     cached = first || new
-    ['office365_key', 'office365_secret', 'google_oauth_key', 'google_oauth_secret'].each do |key|
+    ['office365_key', 'office365_secret', 'google_oauth_key', 'google_oauth_secret', 'failed_password_attempts', 'lockout_period', 'permanent_lock'].each do |key|
       available_settings["#{key.upcase}"] = { 'default' => ENV["#{key.upcase}"], 'cached' => cached.send(key) || '' }
     end
   end
