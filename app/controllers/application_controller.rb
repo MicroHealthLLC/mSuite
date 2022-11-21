864f99e72f739a3bdcd9c602253d1605ef9ae6af
zachegnermh <zach.egner@microhealthllc.com>
2022-11-21 16:45:16 -0500

    fix csfr error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
