b97853b17192efefd02a2f73c3a8906055b7d4fb
b-solution <bilel.kedidi@gmail.com>
2023-02-23 16:26:22 +0500

    Fixed File Transfer map: Clicking reload/refresh should not remove the file(s) to download

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -7,14 +7,14 @@ class FilesController < ApplicationController
     send_file Rails.root.join('public', 'uploads', params[:filename]), type: 'application/octet-stream', x_sendfile: true
   end
 
-  def file_downloaded
-    filename = params[:filename]
-    if FileManagementService.new(filename, 'download').delete_file
-      render json: { message: "File downloaded successfully" }
-    else
-      render json: { error: "Could not download file" }, status: :unprocessable_entity
-    end
-  end
+  # def file_downloaded
+  #   filename = params[:filename]
+  #   if FileManagementService.new(filename, 'download').delete_file
+  #     render json: { message: "File downloaded successfully" }
+  #   else
+  #     render json: { error: "Could not download file" }, status: :unprocessable_entity
+  #   end
+  # end
 
   def file_canceled
     filename = params[:filename]
