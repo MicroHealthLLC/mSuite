0cb6eb60b2af286df0bf7a47dcae669cdf9fffcd
b-solution <bilel.kedidi@gmail.com>
2024-04-15 16:23:17 +0500

    resolve time range event issue

--- app/javascript/components/todo/TodoMap.vue
+++ app/javascript/components/todo/TodoMap.vue
@@ -1,275 +1,131 @@
 <template>
   <div>
-    <b-list-group-item
-      :id="'todo' + node.id"
-      tabindex="0"
-      @click="myTodo(node)"
-      class="mb-0"
-    >
+    <b-list-group-item :id="'todo' + node.id" tabindex="0" @click="myTodo(node)" class="mb-0">
       <div class="flex" v-if="selectedTodo.id != node.id">
         <div class="col-6 d-flex flex-row">
           <div class="flex-auto">
-            <input
-              type="checkbox"
-              class="mr-4"
-              :checked="node.is_disabled"
-              @input="checkCheckedAns($event)"
-              @click="updateTodo(node, node.name, !node.is_disabled)"
-            />
+            <input type="checkbox" class="mr-4" :checked="node.is_disabled" @input="checkCheckedAns($event)"
+              @click="updateTodo(node, node.name, !node.is_disabled)" />
           </div>
-          <label
-            v-if="selectedTodo.id != node.id"
-            @click="showInputFieldToggle(node)"
-            class="mb-0 text-wrap"
-            :for="'todo-' + node.id"
-            :class="{ 'line-through': node.is_disabled }"
-            >{{ node.name }}</label
-          >
+          <label v-if="selectedTodo.id != node.id" @click="showInputFieldToggle(node)" class="mb-0 text-wrap"
+            :for="'todo-' + node.id" :class="{ 'line-through': node.is_disabled }">{{ node.name }}</label>
         </div>
-        <span
-          @click="showInputFieldToggle(node)"
-          class="dueDate col-3"
-          :class="{ 'line-through': node.is_disabled }"
-        >
+        <span @click="showInputFieldToggle(node)" class="dueDate col-3" :class="{ 'line-through': node.is_disabled }">
           {{ showStartDate }}
         </span>
-        <span
-          v-if="selectedTodo.id != node.id && node"
-          @click="showInputFieldToggle(node)"
-          class="dueDate col-2"
-          :class="{ 'line-through': node.is_disabled }"
-        >
+        <span v-if="selectedTodo.id != node.id && node" @click="showInputFieldToggle(node)" class="dueDate col-2"
+          :class="{ 'line-through': node.is_disabled }">
           {{
-            node.duedate
-              ? `Due: ${new Date(node.duedate).toLocaleDateString("en-US")}`
-              : ""
-          }}
+      node.duedate
+        ? `Due: ${new Date(node.duedate).toLocaleDateString("en-US")}`
+        : ""
+    }}
         </span>
         <div class="col-1 d-flex flex-row align-items-end">
-          <i
-            v-b-tooltip.hover
-            title="Add Subtask"
-            class="ml-lg-3 fa fa-plus addTodo"
-            @click="toggleChildModal(node)"
-          ></i>
-          <i
-            v-b-tooltip.hover
-            title="Delete Todo"
-            class="ml-lg-3 ml-xs-1 fa fa-times deleteTodo"
-            @click="toggleDeleteTodo(node)"
-          ></i>
+          <i v-b-tooltip.hover title="Add Subtask" class="ml-lg-3 fa fa-plus addTodo"
+            @click="toggleChildModal(node)"></i>
+          <i v-b-tooltip.hover title="Delete Todo" class="ml-lg-3 ml-xs-1 fa fa-times deleteTodo"
+            @click="toggleDeleteTodo(node)"></i>
         </div>
       </div>
       <div class="ml-3" v-else-if="selectedTodo.id == node.id">
         <div class="relative flex h-full">
           <div class="container relative z-20 max-w-xl mt-20 h-min">
-            <b-form
-              @submit.prevent="
-                updateTodo(
-                  selectedTodo,
-                  selectedTodo.name,
-                  selectedTodo.is_disabled
-                )
-              "
-            >
+            <b-form @submit.prevent="updateTodo(selectedTodo, selectedTodo.name, selectedTodo.is_disabled)">
               <b-row>
                 <b-col cols="6" sm="6">
-                  <b-form-input
-                    v-model="selectedTodo.name"
-                    ref="title"
-                    type="text"
-                    placeholder="Your Todo"
-                  >
+                  <b-form-input v-model="selectedTodo.name" ref="title" type="text" placeholder="Your Todo">
                   </b-form-input>
                 </b-col>
                 <b-col cols="4" sm="4" class="w-50 d-flex flex-row">
-                  <date-picker
-                    id="input"
-                    class="w-100"
-                    v-model="selectedTodoRange"
-                    :format="format"
-                    range
-                    range-separator=" - "
-                  ></date-picker>
+                  <date-picker id="input" class="w-100" v-model="selectedTodoRange" :format="format" range disabled-time
+                    range-separator=" - "></date-picker>
                 </b-col>
                 <b-col sm="2" cols="2" class="d-flex flex-row">
-                  <b-button
-                    v-b-tooltip.hover
-                    title="Save"
-                    type="submit"
-                    variant="success"
-                  >
-                    <i class="fas fa-save"></i
-                  ></b-button>
-                  <b-button
-                    class="ml-1"
-                    v-b-tooltip.hover
-                    title="Cancel"
-                    variant="secondary"
-                    @click="clearTodoEditObj"
-                    ><i class="fa fa-ban"></i
-                  ></b-button>
+                  <b-button v-b-tooltip.hover title="Save" type="submit" variant="success">
+                    <i class="fas fa-save"></i></b-button>
+                  <b-button class="ml-1" v-b-tooltip.hover title="Cancel" variant="secondary"
+                    @click="clearTodoEditObj"><i class="fa fa-ban"></i></b-button>
                 </b-col>
               </b-row>
             </b-form>
           </div>
-          <div
-            @click="
-              updateTodo(
-                selectedTodo,
-                selectedTodo.name,
-                selectedTodo.is_disabled
-              )
-            "
-            class="absolute top-0 z-10 w-full h-full"
-          ></div>
+          <div @click="updateTodo(selectedTodo, selectedTodo.name, selectedTodo.is_disabled)"
+            class="absolute top-0 z-10 w-full h-full"></div>
         </div>
       </div>
     </b-list-group-item>
     <!-- Black background color added to see draggable field for debugging -->
     <div style="background-color: black">
-      <draggable
-        class="list-group"
-        group="child"
-        :disabled="dragLocked"
-        :list="sortedChildTodos"
-        :move="checkMove"
-        @change="(e) => handleEndChild(e, sortedChildTodos, node)"
-        @start="drag = true"
-        @end="(e) => onDragEnd(e, sortedChildTodos, node)"
-        v-bind="dragOptions"
-      >
+      <draggable class="list-group" group="child" :disabled="dragLocked" :list="sortedChildTodos" :move="checkMove"
+        @change="(e) => handleEndChild(e, sortedChildTodos, node)" @start="drag = true"
+        @end="(e) => onDragEnd(e, sortedChildTodos, node)" v-bind="dragOptions">
         <transition-group type="transition" :name="!drag ? 'list' : null">
-          <b-list-group-item
-            class="pl-5 mb-0"
-            v-for="child in sortedChildTodos"
-            :node="child"
-            :key="child.id"
-          >
+          <b-list-group-item class="pl-5 mb-0" v-for="child in sortedChildTodos" :node="child" :key="child.id">
             <div class="flex" v-if="selectedTodo.id != child.id">
               <div class="col-6 d-flex flex-row">
                 <div class="flex-auto">
-                  <input
-                    type="checkbox"
-                    class="mr-4"
-                    :checked="child.is_disabled"
-                    @click="updateTodo(child, child.name, !child.is_disabled)"
-                  />
+                  <input type="checkbox" class="mr-4" :checked="child.is_disabled"
+                    @click="updateTodo(child, child.name, !child.is_disabled)" />
                 </div>
-                <label
-                  @click="showInputFieldToggle(child)"
-                  class="mb-0 text-wrap"
-                  :for="'child-' + child.id"
-                  :class="{ 'line-through': child.is_disabled }"
-                  >{{ child.name }}</label
-                >
+                <label @click="showInputFieldToggle(child)" class="mb-0 text-wrap" :for="'child-' + child.id"
+                  :class="{ 'line-through': child.is_disabled }">{{ child.name }}</label>
               </div>
-              <span
-                v-if="selectedTodo.id != child.id"
-                @click="showInputFieldToggle(child)"
-                class="col-3 dueDate pl-1"
-                :class="{ 'line-through': child.is_disabled }"
-              >
+              <span v-if="selectedTodo.id != child.id" @click="showInputFieldToggle(child)" class="col-3 dueDate pl-1"
+                :class="{ 'line-through': child.is_disabled }">
                 {{
-                  child.startdate &&
-                  child.startdate != "" &&
-                  child.startdate != child.duedate
-                    ? `Start: ${new Date(child.startdate).toLocaleDateString(
-                        "en-US"
-                      )}`
-                    : ""
-                }}
+      child.startdate &&
+        child.startdate != "" &&
+        child.startdate != child.duedate
+        ? `Start: ${new Date(child.startdate).toLocaleDateString(
+          "en-US"
+        )}`
+        : ""
+    }}
               </span>
               <span v-else class="col-3"></span>
-              <span
-                @click="showInputFieldToggle(child)"
-                class="col-2 dueDate pl-1"
-                :class="{ 'line-through': child.is_disabled }"
-              >
+              <span @click="showInputFieldToggle(child)" class="col-2 dueDate pl-1"
+                :class="{ 'line-through': child.is_disabled }">
                 {{
-                  child.duedate
-                    ? `Due: ${new Date(child.duedate).toLocaleDateString(
-                        "en-US"
-                      )}`
-                    : ""
-                }}
+      child.duedate
+        ? `Due: ${new Date(child.duedate).toLocaleDateString(
+          "en-US"
+        )}`
+        : ""
+    }}
               </span>
-              <div
-                class="col-1 ml-xl-5 ml-lg-5 ml-md-4 ml-sm-4 ml-4 pl-xl-1 pl-lg-0 pl-md-1 pl-sm-1 pl-1"
-              >
+              <div class="col-1 ml-xl-5 ml-lg-5 ml-md-4 ml-sm-4 ml-4 pl-xl-1 pl-lg-0 pl-md-1 pl-sm-1 pl-1">
                 <i class="d-none ml-2 fa fa-times"></i>
-                <i
-                  v-b-tooltip.hover
-                  placement="top"
-                  title="Delete Todo"
-                  class="mt-1 ml-2 fa fa-times deleteTodo"
-                  @click="toggleDeleteTodo(child)"
-                ></i>
+                <i v-b-tooltip.hover placement="top" title="Delete Todo" class="mt-1 ml-2 fa fa-times deleteTodo"
+                  @click="toggleDeleteTodo(child)"></i>
               </div>
             </div>
             <div v-else-if="selectedTodo.id == child.id">
               <div class="relative flex h-full">
                 <div class="container relative z-20 max-w-xl mt-20 h-min">
-                  <b-form
-                    @submit.prevent="
-                      updateTodo(
-                        selectedTodo,
-                        selectedTodo.name,
-                        selectedTodo.is_disabled
-                      )
-                    "
-                  >
+                  <b-form @submit.prevent="updateTodo(selectedTodo, selectedTodo.name, selectedTodo.is_disabled)">
                     <b-row>
                       <b-col sm="6" cols="6">
-                        <b-form-input
-                          v-model="selectedTodo.name"
-                          ref="title"
-                          type="text"
-                          placeholder="Your Todo"
-                        >
+                        <b-form-input v-model="selectedTodo.name" ref="title" type="text" placeholder="Your Todo">
                         </b-form-input>
                       </b-col>
                       <b-col cols="4" sm="4" class="w-50 d-flex flex-row">
-                        <date-picker
-                          id="input"
-                          class="w-100"
-                          v-model="selectedChildTodoRange"
-                          :format="format"
-                          range
-                          range-separator=" - "
-                        ></date-picker>
+                        <date-picker id="input" class="w-100" v-model="selectedChildTodoRange" :format="format" range
+                          disabled-time range-separator=" - "></date-picker>
                       </b-col>
                       <b-col sm="2" cols="2" class="d-flex flex-row">
-                        <b-button
-                          v-b-tooltip.hover
-                          title="Save"
-                          type="submit"
-                          variant="success"
-                        >
+                        <b-button v-b-tooltip.hover title="Save" type="submit" variant="success">
                           <i class="fas fa-save"></i>
                         </b-button>
-                        <b-button
-                          v-b-tooltip.hover
-                          title="Cancel"
-                          class="ml-1"
-                          variant="secondary"
-                          @click="clearTodoEditObj"
-                          ><i class="fa fa-ban"></i
-                        ></b-button>
+                        <b-button v-b-tooltip.hover title="Cancel" class="ml-1" variant="secondary"
+                          @click="clearTodoEditObj"><i class="fa fa-ban"></i></b-button>
                       </b-col>
                     </b-row>
                   </b-form>
                 </div>
-                <div
-                  @click="
-                    updateTodo(
-                      selectedTodo,
-                      selectedTodo.name,
-                      selectedTodo.is_disabled
-                    )
-                  "
-                  class="absolute top-0 z-10 w-full h-full"
-                ></div>
+                <div @click="updateTodo(selectedTodo, selectedTodo.name, selectedTodo.is_disabled)"
+                  class="absolute top-0 z-10 w-full h-full">
+                </div>
               </div>
             </div>
           </b-list-group-item>
@@ -411,16 +267,24 @@ export default {
       calendarClear.style.display = "none";
     },
     updateTodo(todo, title, completed) {
+      let prevStartDate, prevDueDate
+      prevStartDate = todo.startdate
+      prevDueDate = todo.duedate
+
       if (
         todo.duedate != undefined &&
         todo.duedate !== "" &&
         todo.duedate.getTime != undefined
       ) {
-        todo.duedate = new Date(
-          todo.duedate.getTime() - todo.duedate.getTimezoneOffset() * 60000
-        )
-          .toISOString()
-          .split("T")[0];
+        const prevDueDateTime = new Date(prevDueDate);
+        const newDueDateTime = new Date(todo.duedate);
+
+        // Set new date's time to previous date's time
+        newDueDateTime.setHours(prevDueDateTime.getHours());
+        newDueDateTime.setMinutes(prevDueDateTime.getMinutes());
+        newDueDateTime.setSeconds(prevDueDateTime.getSeconds());
+
+        todo.duedate = newDueDateTime;
       }
 
       if (
@@ -428,13 +292,18 @@ export default {
         todo.startdate !== "" &&
         todo.startdate.getTime != undefined
       ) {
-        todo.startdate = new Date(
-          todo.startdate.getTime() - todo.startdate.getTimezoneOffset() * 60000
-        )
-          .toISOString()
-          .split("T")[0];
+        const prevStartDateTime = new Date(prevStartDate);
+        const newStartDateTime = new Date(todo.startdate);
+
+        // Set new date's time to previous date's time
+        newStartDateTime.setHours(prevStartDateTime.getHours());
+        newStartDateTime.setMinutes(prevStartDateTime.getMinutes());
+        newStartDateTime.setSeconds(prevStartDateTime.getSeconds());
+
+        todo.startdate = newStartDateTime;
       }
-      this.$emit("updateTodo", todo, title, completed);
+
+      this.$emit("updateTodo", todo, title, completed, prevDueDate, prevStartDate);
     },
     myTodo(todo) {
       if (this.selectedTodo.id && todo.id != this.selectedTodo.id) {
@@ -466,6 +335,7 @@ export default {
       }
     },
     showInputField(todo) {
+
       let nod = this.currentMindMap.nodes.find((n) => n.id == todo.id);
       const sta = new Date(nod.startdate);
       const due = new Date(nod.duedate);
@@ -498,6 +368,23 @@ export default {
       if (event.target.checked) event.target.setAttribute("checked", "checked");
       else event.target.removeAttribute("checked");
     },
+    setPreviousTime(dateRange) {
+
+      const originalStartTime = this.selectedTodo.startdate;
+      const originalEndTime = this.selectedTodo.duedate;
+      const startDate = new Date(dateRange[0]);
+      const endDate = new Date(dateRange[1]);
+      if (typeof originalStartTime !== 'string') {
+        startDate.setHours(originalStartTime.getHours());
+        startDate.setMinutes(originalStartTime.getMinutes());
+        startDate.setSeconds(originalStartTime.getSeconds());
+        endDate.setHours(originalEndTime.getHours());
+        endDate.setMinutes(originalEndTime.getMinutes());
+        endDate.setSeconds(originalEndTime.getSeconds());
+      }
+      this.selectedTodo.startdate = startDate;
+      this.selectedTodo.duedate = endDate;
+    },
   },
   computed: {
     sortedChildTodos() {
@@ -522,8 +409,8 @@ export default {
         const dueDate = moment(this.node.duedate);
         return this.node.startdate && !startDate.isSame(dueDate)
           ? `Start: ${new Date(this.node.startdate).toLocaleDateString(
-              "en-US"
-            )}`
+            "en-US"
+          )}`
           : "";
       }
     },
@@ -535,17 +422,20 @@ export default {
       },
       deep: true,
     },
-    selectedTodo() {},
+    selectedTodo() { },
     selectedTodoRange() {
       if (this.selectedTodoRange) {
-        this.selectedTodo.startdate = this.selectedTodoRange[0];
-        this.selectedTodo.duedate = this.selectedTodoRange[1];
+
+        this.setPreviousTime(this.selectedTodoRange)
+        // this.selectedTodo.startdate = this.selectedTodoRange[0];
+        // this.selectedTodo.duedate = this.selectedTodoRange[1];
       }
     },
     selectedChildTodoRange() {
       if (this.selectedChildTodoRange) {
-        this.selectedTodo.startdate = this.selectedChildTodoRange[0];
-        this.selectedTodo.duedate = this.selectedChildTodoRange[1];
+        this.setPreviousTime(this.selectedChildTodoRange)
+        // this.selectedTodo.startdate = this.selectedChildTodoRange[0];
+        // this.selectedTodo.duedate = this.selectedChildTodoRange[1];
       }
     },
   },
